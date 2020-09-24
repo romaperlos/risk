f88fce62c98d0429937802328aeea0054b802d85
Roma RedDragon <roma.perlos@gmail.com>
2020-09-24 10:56:41 +0300

    Fix2 for Heroku

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import './misc/db.js'
 import indexRouter from './routes/index.js'
 import authRouter from './routes/auth.js'
 import riskListRouter from './routes/riskList.js' // добавил импорт на новый роутер реестра рисков (Антон)
-import privateRouter from './routes/private.js'
 
 import userMiddleware from './middlewares/user.js'
 import adminMiddleware from './middlewares/admin.js'
@@ -46,8 +45,6 @@ app.use(adminMiddleware)
 app.use(indexRouter)
 app.use(authRouter)
 app.use('/riskList', riskListRouter) // добавил новый роутер реестра рисков (Антон)
-app.use('/private', privateRouter)
-
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
