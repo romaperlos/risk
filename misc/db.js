86a0401e57e038a2ada9cebb1ffdab57e13fa5a6
Roma RedDragon <roma.perlos@gmail.com>
2020-09-24 12:20:27 +0300

    Fix5

--- misc/db.js
+++ misc/db.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 
+// export const connection = mongoose.createConnection();
+
 export default mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
